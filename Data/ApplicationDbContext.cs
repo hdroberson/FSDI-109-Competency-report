68171ef5e1937fe12e5f4d7898f3e0e825be9385
Harold Roberson <hdroberson@gmail.com>
2019-03-04 20:01:19 -0600

    Add files via upload

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using WebSQlite.Models;
+
+namespace WebSQlite.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Student> Students { get; set; }
+    }
+}
---
+++ Models/Student.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebSQlite.Models
+{
+    public class Student
+    {
+       public int StudentId { get; set; }
+       public string FirstName{ get; set; }
+       public string LastName { get; set; }
+       public string School { get; set; }
+       public DateTime StartDate { get; set; }
+    }
+}
